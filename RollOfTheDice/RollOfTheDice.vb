ffc65d5121d46d0e1bfef3595abd0e2e563b4102
Chris Pickens <Cpickens1270@gmail.com>
2023-02-13 08:20:27 -0700

    set name, options, and heading

---
+++ RollOfTheDice/RollOfTheDice.vb
@@ -0,0 +1,17 @@
+'Christopher Pickens
+'RCET0265
+'Spring 2023
+'Roll of the Dice
+'https://github.com/Pickchr2/RollOfTheDice.git
+
+Option Compare Text
+Option Explicit On
+Option Strict On
+
+Imports System
+
+Module RollOfTheDice
+    Sub Main(args As String())
+
+    End Sub
+End Module
