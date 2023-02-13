7c1363680eccb4d83d78fc7c5e540af18786f300
Chris Pickens <Cpickens1270@gmail.com>
2023-02-13 08:32:12 -0700

    accept user input

--- RollOfTheDice/RollOfTheDice.vb
+++ RollOfTheDice/RollOfTheDice.vb
@@ -12,6 +12,25 @@ Imports System
 
 Module RollOfTheDice
     Sub Main(args As String())
+        Dim userInput As String
+        Dim rollCounter(11) As Integer
+        Dim quitFlag = False
 
+        Do Until quitFlag
+            Console.Clear()
+            Console.WriteLine("Enter a whole number to roll two 6-sided dice that number of times.")
+            Console.WriteLine("Press " & Chr(34) & "Q" & Chr(34) & " at anytime to quit.")
+            userInput = Console.ReadLine()
+
+            Select Case userInput
+
+                Case "Q"
+                    quitFlag = True
+
+                Case Else
+
+            End Select
+
+        Loop
     End Sub
 End Module
