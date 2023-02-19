0a0707906663bc147f0692020143e6d4b9f0110b
Chris Pickens <Cpickens1270@gmail.com>
2023-02-19 10:47:02 -0700

    Set range of acceptable numbers

--- RollOfTheDice/RollOfTheDice.vb
+++ RollOfTheDice/RollOfTheDice.vb
@@ -20,8 +20,8 @@ Module RollOfTheDice
         Do Until quitFlag
             Dim resultCounter(11) As Integer
 
-            Console.WriteLine("Enter a whole number to roll two 6-sided dice that number of times.")
-            Console.WriteLine("Press " & Chr(34) & "C" & Chr(34) & " to clear results, or " & Chr(34) & "Q" & Chr(34) & " at anytime to quit.")
+            Console.WriteLine("Enter a whole number between 1,000 and 1,000,000 to roll two 6-sided dice that number of times.")
+            Console.WriteLine("Press " & Chr(34) & "C" & Chr(34) & " to clear results, or " & Chr(34) & "Q" & Chr(34) & " to quit at anytime.")
             userInput = Console.ReadLine()
             Console.WriteLine()
 
@@ -35,24 +35,28 @@ Module RollOfTheDice
                     Try
                         rollCount = CInt(userInput)
 
-                        Console.WriteLine("Generated Numbers")
-                        Console.WriteLine("-----------------------------------------------------------------------------")
-                        For i As Integer = 1 To rollCount
+                        If rollCount >= 1000 And rollCount <= 1000000 Then
+                            Console.WriteLine("Generated Numbers")
+                            Console.WriteLine("-----------------------------------------------------------------------------")
+                            For i As Integer = 1 To rollCount
 
-                            resultCounter(CInt(randomNumber.Next(0, 11))) += 1
+                                resultCounter(CInt(randomNumber.Next(0, 11))) += 1
 
-                        Next
+                            Next
 
-                        For j As Integer = 2 To 12
-                            Console.Write(CStr(j).PadLeft(6) & "|")
-                        Next
-                        Console.WriteLine()
-                        Console.WriteLine("-----------------------------------------------------------------------------")
-                        For k As Integer = 0 To 10
-                            Console.Write(CStr(resultCounter(k)).PadLeft(6) & "|")
-                        Next
-                        Console.WriteLine()
-                        Console.WriteLine("-----------------------------------------------------------------------------")
+                            For j As Integer = 2 To 12
+                                Console.Write(CStr(j).PadLeft(6) & "|")
+                            Next
+                            Console.WriteLine()
+                            Console.WriteLine("-----------------------------------------------------------------------------")
+                            For k As Integer = 0 To 10
+                                Console.Write(CStr(resultCounter(k)).PadLeft(6) & "|")
+                            Next
+                            Console.WriteLine()
+                            Console.WriteLine("-----------------------------------------------------------------------------")
+                        Else
+                            Console.WriteLine($"Sorry, {rollCount} is out of range of accetable numbers.")
+                        End If
                     Catch ex As Exception
                         Console.WriteLine($"Sorry, {userInput} is not a whole number.")
                     End Try
